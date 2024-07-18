356012c6c7697b70dcba6831b3a6867d129affd4
ElGg01 <jcng.aa135@gmail.com>
2024-07-18 17:19:49 -0600

    Renamed files

--- stack.py
+++ stack.py
@@ -1,21 +1,25 @@
 class Stack:
     def __init__(self) -> None:
-        pass
-
-    stack = []
+        self.stack = []
 
     def add(self, element: int):
+        print("Element added: ", element)
         self.stack.append(element)
 
     def remove(self) -> int:
+        print("Element: ", self.stack[-1], " ", "removed")
         return self.stack.pop()
 
     def show(self):
-        print(self.stack)
+        print("")
+        for element in self.stack:
+            print("│", element, "│")
+        print("─────")
 
 
 stack = Stack()
 stack.add(5)
 stack.add(10)
+stack.show()
 stack.remove()
 stack.show()
