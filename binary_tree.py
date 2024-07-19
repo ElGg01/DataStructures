f0c4d5b7c39b8320a51ef8d14c3be23dbba05c09
ElGg01 <jcng.aa135@gmail.com>
2024-07-19 17:43:00 -0600

    Binary tree refactor

--- binary_tree.py
+++ binary_tree.py
@@ -1,181 +1,98 @@
-class Node:
-    def __init__(self, value: int) -> None:
-        self.parent = None
-        self.left_child = None
-        self.right_child = None
-        self.value = value
+class BinarySearchTreeNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
 
-
-class BinaryTree:
-    def __init__(self) -> None:
-        self.root = None
-
-    def showBinaryTree(self, indentation: int = 4) -> None:
-        if self.root == None:
-            print("Tree empty")
+    def add_child(self, data):
+        if data == self.data:
             return
 
-        def traverseBinaryTree(node: Node, string: str) -> None:
-            print(str(node.value))
-            # Left child:
-            if node.left_child != None:
-                if node.right_child != None:
-                    print(string + "├" + "─" * indentation, end="")
-                else:
-                    print(string + "└" + "─" * indentation, end="")
-                traverseBinaryTree(node.left_child, string + "│" + " " * indentation)
-            if node.right_child != None:
-                print(string + "└" + "─" * indentation, end="")
-                traverseBinaryTree(node.right_child, string + "│" + " " * indentation)
-
-        traverseBinaryTree(self.root, "")
-
-    # METHODS FOR BST TREE
-
-    def insert(self, value: int) -> None:
-        y = None
-        x = self.root
-        n = Node(value)
-
-        while x != None:
-            y = x
-            if n.value <= x.value:
-                x = x.left_child
+        if data < self.data:
+            # add data in the left subtree
+            if self.left:
+                self.left.add_child(data)
             else:
-                x = x.right_child
-
-        n.parent = y
-
-        if y == None:
-            self.root = n
-        else:
-            if n.value <= y.value:
-                y.left_child = n
+                self.left = BinarySearchTreeNode(data)
+        elif data > self.data:
+            # add data in the right subtree
+            if self.right:
+                self.right.add_child(data)
             else:
-                y.right_child = n
+                self.right = BinarySearchTreeNode(data)
 
-    def showBST(self, indentation: int = 4) -> None:
-        if self.root == None:
-            print("Empty tree")
-            return
+    def in_order_traversal(self):
+        elements = []
 
-        def transverseBST(node: Node, string: str) -> None:
-            print(str(node.value))
-            if node.left_child != None:
-                if node.right_child != None:
-                    print(string + "├" + "─" * indentation, end="")
-                    transverseBST(node.left_child, string + "│" + " " * indentation)
-                else:
-                    print(string + "└" + "─" * indentation, end="")
-                    transverseBST(node.left_child, string + " " * (indentation + 1))
+        # Visit left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
 
-            if node.right_child != None:
-                print(string + "└" + "─" * indentation, end="")
-                transverseBST(node.right_child, string + " " * (indentation + 1))
+        # Visit base node
+        elements.append(self.data)
 
-        transverseBST(self.root, "")
+        # visit right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
 
-    def search(self, value: int) -> Node:
-        node = self.root
-        while node != None and node.value != value:
-            if value < node.value:
-                node = node.left_child
+        return elements
+
+    def pre_order_traversal(self):
+        elements = []
+
+        elements.append(self.data)
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def search(self, value):
+        if self.data == value:
+            return True
+
+        if value < self.data:
+            if self.left:
+                return self.left.search(value)
             else:
-                node = node.right_child
-        return node
+                return False
+
+        if value > self.data:
+            if self.right:
+                return self.right.search(value)
+            else:
+                return False
 
 
-# TEST
-tree = BinaryTree()
+def build_tree(elements) -> BinarySearchTreeNode:
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
 
-node1 = Node(5)
-tree.root = node1
-
-node2 = Node(100)
-node1.left_child = node2
-node2.parent = node1
-
-node3 = Node(3)
-node3.parent = node1
-node1.right_child = node3
-
-node4 = Node(20)
-node4.parent = node2
-node2.left_child = node4
-
-node5 = Node(10)
-node5.parent = node2
-node2.right_child = node5
-
-node6 = Node(7)
-node6.parent = node4
-node4.left_child = node6
-
-node7 = Node(4)
-node7.parent = node4
-node4.right_child = node7
-
-tree.showBinaryTree()
+    return root
 
 
-# Get the level of a node
-def getNodeLevel(node: Node) -> None:
-    if node.parent == None:
-        return 0
-    else:
-        return 1 + getNodeLevel(node.parent)
+if __name__ == "__main__":
+    numbers = [17, 4, 1, 20, 9, 24, 18, 34]
+    numbers_tree = build_tree(numbers)
 
-
-print("Nivel del nodo: " + str(getNodeLevel(node7)))
-
-
-def getTreeHeight(tree: BinaryTree) -> int:
-
-    def getHeight(node: Node) -> int:
-        if node == None:
-            return -1
-        else:
-            return 1 + max(getHeight(node.left_child), getHeight(node.right_child))
-
-    return getHeight(tree.root)
-
-
-print("Altura del arbol: " + str(getTreeHeight(tree)))
-
-
-def getTreeNodes(tree: BinaryTree) -> int:
-
-    def getTotalNodes(node: Node) -> int:
-        if node == None:
-            return 0
-        else:
-            return 1 + getTotalNodes(node.left_child) + getTotalNodes(node.right_child)
-
-    return getTotalNodes(tree.root)
-
-
-print("Numero total de nodos: " + str(getTreeNodes(tree)))
-
-
-# Create Binary search trees
-def createBST(array: list) -> BinaryTree:
-    bst = BinaryTree()
-    for i in array:
-        bst.insert(i)
-    return bst
-
-
-print("ARBOL BST:")
-
-array = [5, 1, 4, 7, 8, 2, 9, 0, 3, 6]
-bst = createBST(array)
-bst.showBST()
-
-
-# SEARCH:
-valueToSearch = 8
-a = bst.search(valueToSearch)
-if a != None:
-    print(f"Se encontro el valor {valueToSearch} en el arbol")
-else:
-    print(f"No existe el elemento {valueToSearch} en el arbol")
+    print(numbers_tree.in_order_traversal())
+    print(numbers_tree.pre_order_traversal())
+    print(numbers_tree.post_order_traversal())
+    print(numbers_tree.search(200))
